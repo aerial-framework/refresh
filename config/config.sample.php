bcf2121dee1b2105a6b84a0c8320d41b2606a5ef
Danny Kopping <dannykopping@gmail.com>
2012-02-23 18:37:06 +0200

    Removed config.php and changed it to config.sample.php

---
+++ config/config.sample.php
@@ -0,0 +1,41 @@
+<?php
+
+    xdebug_disable();
+
+    /**
+     * The path to the Aerial www folder
+     *
+     * @see /projectroot/www
+     */
+    define("WWW_PATH", dirname(__FILE__));
+
+    /**
+     * The path to the Aerial library folder
+     *
+     * @see /projectroot/lib
+     */
+    define("LIBRARY_PATH", dirname(__FILE__) . "/../lib");
+
+    /**
+     * Debug mode - show debugging messages and data if TRUE
+     */
+    define("DEBUG_MODE", true);
+
+    define("DB_ENGINE", "mysql");
+    define("DB_HOST", "localhost");
+    define("DB_SCHEMA", "");
+    define("DB_USERNAME", "aerial");
+    define("DB_PASSWORD", "");
+    define("DB_PORT", "3306");
+
+    define("PHP_MODELS", WWW_PATH."/../src_php/models");
+
+    // check whether LIBRARY PATH is valid, as it will be needed shortly
+    if(!realpath(LIBRARY_PATH))
+        throw new Exception("Library path not valid: ".LIBRARY_PATH);
+
+    function import($classPath)
+    {
+    	$importFile = str_replace(".", DIRECTORY_SEPARATOR, $classPath) . ".php";
+    	require_once(LIBRARY_PATH . DIRECTORY_SEPARATOR . $importFile);
+    }
