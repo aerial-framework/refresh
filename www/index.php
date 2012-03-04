dd15a394e94cd565c760b9e7ea72550ba9d1e39b
Danny Kopping <dannykopping@gmail.com>
2012-03-05 01:09:24 +0200

    Added in `is_undefined` function for backwards compatibility with previous Aerial version - to be moved/deprecated

--- www/index.php
+++ www/index.php
@@ -1,5 +1,13 @@
 <?php
 
+    if(!function_exists("is_undefined"))
+    {
+        function is_undefined($obj)
+        {
+            return is_object($obj) ? get_class($obj) == "undefined" : false;
+        }
+    }
+
     // load the configuration file
     require_once("../config/config.php");
 
