ef78b65023588d18944db0693b61d4bceba05adb
Danny Kopping <dannykopping@gmail.com>
2012-03-11 21:50:50 +0200

    Added routing instructions for AMF requests

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -1,15 +1,18 @@
 <?php
-    require_once(Configuration::get("LIBRARY_PATH")."/aerialframework/core/Bootstrapper.php");
+    require_once(Configuration::get("LIBRARY_PATH") . "/aerialframework/core/Bootstrapper.php");
 
     class Bootstrap
     {
         public static function actAsServer($override = null)
         {
+            // instantiate the Aerial bootstrapper
+            Bootstrapper::getInstance();
+
             $libraryPath = Configuration::get("LIBRARY_PATH");
             if(empty($libraryPath) || !realpath($libraryPath))
                 throw new Exception("LIBRARY_PATH is invalid.");
 
-            if(!empty($override))
+            if(empty($override))
                 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : null;
             else
                 $contentType = $override;
@@ -17,13 +20,11 @@
             switch($contentType)
             {
                 default:
-                case "application/json":
-                    Bootstrapper::getInstance();
-
-                    include_once($libraryPath."/servers/rest/index.php");
+                    include_once($libraryPath . "/servers/rest/index.php");
                     break;
                 case "application/x-amf":
-                    include_once($libraryPath."/servers/amfphp/gateway.php");
+                    import("aerialframework.service.AbstractService");
+                    include_once($libraryPath . "/servers/amfphp2/index.php");
                     break;
             }
         }
