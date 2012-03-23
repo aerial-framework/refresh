e892979c789707bdd020f080899b87a2b50304db
Danny Kopping <dannykopping@gmail.com>
2012-03-24 00:51:37 +0200

    Added documentation to AbstractService.php

--- lib/aerialframework/service/AbstractService.php
+++ lib/aerialframework/service/AbstractService.php
@@ -1,162 +1,193 @@
 <?php
-	abstract class AbstractService
-	{
-		protected $connection;
-		protected $table;
-		protected $modelName;
+    abstract class AbstractService
+    {
+        protected $connection;
+        protected $table;
+        protected $modelName;
 
-		public function __construct()
-		{
-			$this->connection = Doctrine_Manager::connection();
+        public function __construct()
+        {
+            $this->connection = Doctrine_Manager::connection();
 
-			// try connect when create so that any connection errors can be detected early
-			if (!$this->connection->isConnected())
-				$this->connection->connect();
+            // try connect when create so that any connection errors can be detected early
+            if(!$this->connection->isConnected())
+                $this->connection->connect();
 
-			$this->table = $this->connection->getTable($this->modelName);
-		}
+            $this->table = $this->connection->getTable($this->modelName);
+        }
 
-		/**
-		 * Saves an Aerial model - if it does not exist (no identifier provided) then a new one will be created
-		 *
-		 * @param $object					   The object to save
-		 * @param bool $returnCompleteObject	Whether or not to return the complete graph of the saved object
-		 * @param bool $mapToModel			  Whether or not to "serialize" the incoming object to its Doctrine equivalent (internal)
-		 * @return null
-		 */
-		public function save($object, $returnCompleteObject = false, $mapToModel = true)
-		{
-			if ($mapToModel)
-				$object = ModelMapper::mapToModel($this->modelName, $object, true);
+        /**
+         * Saves an Aerial model - if it does not exist (no identifier provided) then a new one will be created
+         *
+         * @param $object array|stdClass            The object to save
+         * @param bool $returnCompleteObject        Whether or not to return the complete graph of the saved object
+         * @param bool $mapToModel                  Whether or not to "serialize" the incoming object to its Doctrine equivalent (internal)
+         *
+         * @return int|Aerial_Record
+         */
+        public function save($object, $returnCompleteObject = false, $mapToModel = true)
+        {
+            if($mapToModel)
+                $object = ModelMapper::mapToModel($this->modelName, $object, true);
 
-			$result = $object->trySave();
+            $result = $object->trySave();
 
-			if ($result === true) {
-				return $returnCompleteObject ? $object : $object->getIdentifier();
-			}
-			else
-			{
-				$object->save();
-			}
+            if($result === true)
+            {
+                return $returnCompleteObject ? $object : $object->getIdentifier();
+            }
+            else
+            {
+                $object->save();
+            }
 
-		}
+        }
 
-		/**
-		 * Alias of "save" function
-		 * @param $object
-		 * @param bool $returnCompleteObject
-		 * @param bool $mapToModel
-		 * @return null
-		 */
-		public function update($object, $returnCompleteObject = false, $mapToModel = true)
-		{
-			return self::save($object, $returnCompleteObject, $mapToModel);
-		}
+        /**
+         * Alias of "save" function
+         *
+         * @param $object array|stdClass            The object to save
+         * @param bool $returnCompleteObject        Whether or not to return the complete graph of the saved object
+         * @param bool $mapToModel                  Whether or not to "serialize" the incoming object to its Doctrine equivalent (internal)
+         *
+         * @return int|Aerial_Record
+         */
+        public function update($object, $returnCompleteObject = false, $mapToModel = true)
+        {
+            return self::save($object, $returnCompleteObject, $mapToModel);
+        }
 
-		public function insert($object, $returnCompleteObject = false, $mapToModel = true)
-		{
-			if ($mapToModel)
-				$object = ModelMapper::mapToModel($this->modelName, $object);
+        /**
+         * Saves an Aerial model - the identifier will be ignored and a new record will be created
+         *
+         * @param $object array|stdClass            The object to save
+         * @param bool $returnCompleteObject        Whether or not to return the complete graph of the saved object
+         * @param bool $mapToModel                  Whether or not to "serialize" the incoming object to its Doctrine equivalent (internal)
+         *
+         * @return int|Aerial_Record
+         */
+        public function insert($object, $returnCompleteObject = false, $mapToModel = true)
+        {
+            if($mapToModel)
+                $object = ModelMapper::mapToModel($this->modelName, $object);
 
-			// unset the primary key values if one is set to insert a new record
-			foreach ($object->table->getIdentifierColumnNames() as $primaryKey)
-				unset($object->$primaryKey);
+            // unset the primary key values if one is set to insert a new record
+            foreach($object->table->getIdentifierColumnNames() as $primaryKey)
+                unset($object->$primaryKey);
 
-			$result = $object->trySave();
+            $result = $object->trySave();
 
-			if ($result === true) {
-				return $returnCompleteObject ? $object : $object->getIdentifier();
-			}
-			else
-			{
-				$object->save();
-			}
+            if($result === true)
+            {
+                return $returnCompleteObject ? $object : $object->getIdentifier();
+            }
+            else
+            {
+                $object->save();
+            }
 
-		}
+        }
 
-		public function drop($object, $mapToModel = true)
-		{
-			if ($mapToModel)
-				$object = ModelMapper::mapToModel($this->modelName, $object, true);
+        /**
+         * Deletes an Aerial model
+         *
+         * @param $object array|stdClass            The object to delete
+         * @param bool $mapToModel                  Whether or not to "serialize" the incoming object to its Doctrine equivalent (internal)
+         *
+         * @return mixed
+         */
+        public function drop($object, $mapToModel = true)
+        {
+            if($mapToModel)
+                $object = ModelMapper::mapToModel($this->modelName, $object, true);
 
-			return $object->delete();
-		}
-
-		private function find($criteria, $limit, $offset, $sort, $relations, $hydrateAMF = true)
-		{
-			$q = Doctrine_Query::create()->from("$this->modelName r");
-
-			//========================  Selects / Joins ==========================
-			if ($relations) {
-				//Merge the relations into a single tree; validate all paths start with the root table.
-				$mergedRelations = array();
-				foreach ($relations as $path)
-				{
-					list($dirty_key) = explode(".", $path, 2);
-					if (Aerial_Relationship::key($dirty_key) <> $this->modelName) $path = $this->modelName . "." . $path;
-					$mergedRelations = Aerial_Relationship::merge($mergedRelations, $path);
-				}
-
-				//Build the DQL 'leftJoin' and 'Select' parts.
-				$relationParts = Aerial_Relationship::relationParts($mergedRelations);
-				foreach ($relationParts["joins"] as $join)
-					$q->leftJoin($join);
-
-				$q->select($relationParts["selects"]);
-			}
-
-			//============================  Criteria =============================
-			if ($criteria) {
-				foreach ($criteria as $key=> $value)
-					$q->addWhere("r.$key =?", $value);
-			}
-
-			if ($relationParts && !$relationParts["criteria"])
-				$relationParts["criteria"] = array();
-
-			if ($relationParts) {
-				foreach ($relationParts["criteria"] as $criteria)
-					$q->addWhere($criteria);
-			}
+            return $object->delete();
+        }
 
 
-			//============================   Order  ===============================
-			if ($sort) {
-				foreach ($sort as $key=> $value)
-				{
-					$q->addOrderBy("$key $value");
-				}
-			}
+        private function find($criteria, $limit, $offset, $sort, $relations, $hydrateAMF = true)
+        {
+            $q = Doctrine_Query::create()->from("$this->modelName r");
 
-			//==========================  Pagination  ==============================
-			if ($limit) $q->limit($limit);
-			if ($offset) $q->offset($offset);
+            //========================  Selects / Joins ==========================
+            if($relations)
+            {
+                //Merge the relations into a single tree; validate all paths start with the root table.
+                $mergedRelations = array();
+                foreach($relations as $path)
+                {
+                    list($dirty_key) = explode(".", $path, 2);
+                    if(Aerial_Relationship::key($dirty_key) <> $this->modelName) $path = $this->modelName . "." . $path;
+                    $mergedRelations = Aerial_Relationship::merge($mergedRelations, $path);
+                }
 
-			$q->setHydrationMode($hydrateAMF ? Aerial_Core::HYDRATE_AMF_COLLECTION : Aerial_Core::HYDRATE_RECORD);
-			$results = $q->execute();
+                //Build the DQL 'leftJoin' and 'Select' parts.
+                $relationParts = Aerial_Relationship::relationParts($mergedRelations);
+                foreach($relationParts["joins"] as $join)
+                    $q->leftJoin($join);
 
-			return $results;
+                $q->select($relationParts["selects"]);
+            }
 
-		}
+            //============================  Criteria =============================
+            if($criteria)
+            {
+                foreach($criteria as $key=> $value)
+                    $q->addWhere("r.$key =?", $value);
+            }
+
+            if($relationParts && !$relationParts["criteria"])
+                $relationParts["criteria"] = array();
+
+            if($relationParts)
+            {
+                foreach($relationParts["criteria"] as $criteria)
+                    $q->addWhere($criteria);
+            }
 
 
-		public function count()
-		{
-			return $this->table->count();
-		}
+            //============================   Order  ===============================
+            if($sort)
+            {
+                foreach($sort as $key=> $value)
+                {
+                    $q->addOrderBy("$key $value");
+                }
+            }
 
-		private function query($properties)
-		{
-			$q = Doctrine_Query::create();
-			foreach ($properties as $property)
-			{
-				$method = $property["key"];
-				call_user_func_array(array($q, $method), $property["value"]);
-			}
+            //==========================  Pagination  ==============================
+            if($limit) $q->limit($limit);
+            if($offset) $q->offset($offset);
 
-			$q->setHydrationMode(Aerial_Core::HYDRATE_AMF_COLLECTION);
-			return $q->execute();
-		}
-	}
+            $q->setHydrationMode($hydrateAMF ? Aerial_Core::HYDRATE_AMF_COLLECTION : Aerial_Core::HYDRATE_RECORD);
+            $results = $q->execute();
+
+            return $results;
+
+        }
+
+        /**
+         * Count the number of records of the related table to this service
+         *
+         * @return int
+         */
+        public function count()
+        {
+            return $this->table->count();
+        }
+
+        private function query($properties)
+        {
+            $q = Doctrine_Query::create();
+            foreach($properties as $property)
+            {
+                $method = $property["key"];
+                call_user_func_array(array($q, $method), $property["value"]);
+            }
+
+            $q->setHydrationMode(Aerial_Core::HYDRATE_AMF_COLLECTION);
+            return $q->execute();
+        }
+    }
 
 ?>
